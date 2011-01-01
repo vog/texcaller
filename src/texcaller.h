6d9cfe8f28008fe15df437d20ec1a86d5a2a701d
Volker Grabsch <vog@notjusthosting.com>
2011-01-01 20:14:21 +0100

    update Copyright year

--- src/texcaller.h
+++ src/texcaller.h
@@ -37,7 +37,8 @@ git clone https://github.com/vog/texcaller.git
  *
  *  \page license License
  *
- *  Copyright © 2010 <a href="http://www.profv.de/">Volker Grabsch</a>
+ *  Copyright © 2010, 2011
+ *  <a href="http://www.profv.de/">Volker Grabsch</a>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining
  *  a copy of this software and associated documentation files (the
