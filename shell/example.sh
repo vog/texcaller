f4e926272e7ebf1e0fb1d21998deb41fb88705c0
Volker Grabsch <vog@notjusthosting.com>
2012-01-07 01:01:21 +0100

    fixed Shell example to work with dash

--- shell/example.sh
+++ shell/example.sh
@@ -1,2 +1,8 @@
-echo '\documentclass{article}\begin{document}Hello world!\end{document}' >hello.tex
+cat >hello.tex <<'EOF'
+\documentclass{article}
+\begin{document}
+Hello world!
+\end{document}
+EOF
+
 texcaller LaTeX PDF 5 <hello.tex >hello.pdf
