d180cc83d6c118ddfa2887924760432e32b82ed7
Volker Diels-Grabsch <v@njh.eu>
2017-09-27 11:51:25 +0200

    Use new-style division in Python example code

--- python/example.py
+++ python/example.py
@@ -1,6 +1,6 @@
 # coding: UTF-8
 
-from __future__ import print_function, unicode_literals
+from __future__ import division, print_function, unicode_literals
 
 import texcaller
 
@@ -10,7 +10,7 @@ Hello world!
 \end{document}'''
 
 pdf, info = texcaller.convert(latex, 'LaTeX', 'PDF', 5)
-print('PDF size:     %.1f KB' % (len(pdf) / 1024.0))
+print('PDF size:     %.1f KB' % (len(pdf) / 1024))
 print('PDF content:  %s ... %s' % (pdf[:5], pdf[-6:]))
 
 s = 'Téxt → "with" $peciäl <characters>'
